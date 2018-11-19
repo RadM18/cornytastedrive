2f1a8a858dd4849f659ef9e8242d222b70fdbe3d
XpertMAN <37657232+XpertMAN@users.noreply.github.com>
2018-11-19 15:22:54 +0300

    corny

---
+++ public_html/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+	<link rel="shortcut icon" href="images/favicon.png">
+    <title>cornyTASTEdrive</title>
+    <meta name="description" content="Выиграйте месячный запас Corny! Сделайте фото с Corny, веселыми табличками от Corny или Cornyмобилем">
+  	<meta name="Keywords" content="Выиграйте месячный запас Corny!">
+	<link href="css/fonts.css?v=1" rel="stylesheet">
+    <link href="css/site.css?v=1" rel="stylesheet">
+  </head>
+<body id="page-home">
+	<div class="container">
+			<div class="main-box">
+
+				<div class="col-lgg-7">
+					  <img class="img-fluid2" src="images/car2.png"> 
+				</div>
+
+				<div class="col-lg-5">
+					<img class="img-fluid" src="images/corny.png">
+					<h1>Выиграйте месячный запас Corny!</h1> 
+					<ul>
+						<li class="li1">сделайте фото с Corny, веселыми табличками от Corny или Cornyмобилем</li>
+						<li class="li2">выложите в Инстаграм с хештегом <strong>#corny<span>TASTE</span>drive</strong></li>
+						<li class="li3">наберите больше лайков в этот день</li>
+						<li class="li4">получите Corny! </li>
+					</ul>
+	  
+					<p class="link-p"><a href="Правила Акции - CornyTasteDrive.pdf" target="_blank">Подробные правила акции — на ссылке</a><p>
+						<br />
+
+	<!-- 					<small>*По данным ООО «ГфК-Русь», Сorny — лидер по объемам продаж (кг) в категории «Злаковые батончики» в 2017 году в России.</small> -->
+     			</div>
+
+     		</div>
+	</div>
+ </body>
+</html>
